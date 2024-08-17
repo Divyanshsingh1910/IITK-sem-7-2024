10289609cdfa17f6c3d4e66992901db193aa3951
Divyansh <singhdivyansh1910@gmail.com>
2024-08-17 15:00:57 +0530

    initial commit

---
+++ cs610/assignments/assign1/matmul-blocking.cpp
@@ -0,0 +1,91 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+using HR = high_resolution_clock;
+using HRTimer = HR::time_point;
+
+#define N (2048)
+
+void matmul_ijk(const uint32_t *A, const uint32_t *B, uint32_t *C, const int SIZE) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      uint32_t sum = 0.0;
+      for (int k = 0; k < SIZE; k++) {
+        sum += A[i * SIZE + k] * B[k * SIZE + j];
+      }
+      C[i * SIZE + j] += sum;
+    }
+  }
+}
+
+void matmul_ijk_blocking(const uint32_t *A, const uint32_t *B, uint32_t *C, const int SIZE) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      uint32_t sum = 0.0;
+      for (int k = 0; k < SIZE; k++) {
+        sum += A[i * SIZE + k] * B[k * SIZE + j];
+      }
+      C[i * SIZE + j] += sum;
+    }
+  }
+}
+
+void init(uint32_t *mat, const int SIZE) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      mat[i * SIZE + j] = 1;
+    }
+  }
+}
+
+void print_matrix(const uint32_t *mat, const int SIZE) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      cout << mat[i * SIZE + j] << "\t";
+    }
+    cout << "\n";
+  }
+}
+
+void check_result(const uint32_t *ref, const uint32_t *opt, const int SIZE) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (ref[i * SIZE + j] != opt[i * SIZE + j]) {
+        assert(false && "Diff found between sequential and blocked versions!\n");
+      }
+    }
+  }
+}
+
+int main() {
+  uint32_t *A = new uint32_t[N * N];
+  uint32_t *B = new uint32_t[N * N];
+  uint32_t *C_seq = new uint32_t[N * N];
+
+  init(A, N);
+  init(B, N);
+  init(C_seq, N);
+
+  HRTimer start = HR::now();
+  matmul_ijk(A, B, C_seq, N);
+  HRTimer end = HR::now();
+  auto duration = duration_cast<microseconds>(end - start).count();
+  cout << "Time without blocking (us): " << duration << "\n";
+
+  uint32_t *C_blk = new uint32_t[N * N];
+  init(C_blk, N);
+
+  start = HR::now();
+  matmul_ijk_blocking(A, B, C_blk, N);
+  end = HR::now();
+  duration = duration_cast<microseconds>(end - start).count();
+  cout << "Time with blocking (us): " << duration << "\n";
+
+  check_result(C_seq, C_blk, N);
+
+  return EXIT_SUCCESS;
+}
