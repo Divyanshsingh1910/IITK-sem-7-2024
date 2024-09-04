aeb999ce1c5aa2a37b06a6759729d8a8520070f4
Divyansh <divyansh@csews180.cse.iitk.ac.in>
2024-09-04 10:51:15 +0530

    sync

--- cs610/assignments/assign2/problem2/problem2.cpp
+++ cs610/assignments/assign2/problem2/problem2.cpp
@@ -16,24 +16,72 @@ using std::cout;
 using std::endl;
 using std::ios;
 
+/*  data structures */
+std::queue<string> buffer;          //memory buffer
 
+/*  variables   */
+uint64_t current_buffer_size;
+uint64_t MAX_BUFFER_SIZE;
+uint64_t THREAD_COUNT;
+
+
+/* locks    */
+//reads from the file
+pthread_mutex_t file_mutext = PTHREAD_MUTEX_INITIALIZER;
+
+//undefined args error
 void print_usage(char *prog_name) {
     cerr << "usage: " << prog_name <<
    " <input file> <producer count> <lines per thread> <buffer size> <output file>\n";
     exit(EXIT_FAILURE);
 }
 
-int thread_count = 0;
+/*  thread routine  */
+void *thread_runner(void *);
 
-int main(int argc, char* argv[])
+int main(int argc, chadr* argv[])
 {
     if(argc != 5){
        print_usage(argv[0]);
     }
 
-    thread_count = strtol(argv[2], NULL, 10);
+    //args read
+    THREAD_COUNT = strtol(argv[2], NULL, 10);
+    std::string input_file = argv[1];
 
-    std::string input = argv[1];
+    //thread spawning
+    pthread_t threads_worker[THREAD_COUNT];
 
+    struct t_data *thread_args = (struct t_data *)malloc(
+            sizeof(struct t_data) * THREAD_COUNT);
+
+    for(int i = 0; i < THREAD_COUNT; i++){
+            thread_args[i].tid = i;
+            pthread_create(&threads_worker[i], nullptr,
+                    thread_runner, (void*)&thread_args[i]);
+    }
+
+
+
+    //barrier sync
+    for(int i=0; i < THREAD_COUNT; i++)
+            pthread_join(threads_worker[i], NULL);
+
+
+    return EXIT_SUCCESS;
 
 }
+
+
+void *thread_runner( void* th_args){
+    struct t_data *args = (struct t_data *)th_args;
+    uint32_t thread_id = args->tid;
+
+}
+
+
+
+
+
+
+
